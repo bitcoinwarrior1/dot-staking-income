2bd49b4e712dd42146558e8610a1fbd12fba515d
James Sangalli <j.l.sangalli@gmail.com>
2021-07-19 20:14:00 +1000

    refactor into helper class

--- app.js
+++ app.js
@@ -1,34 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const request = require("superagent");
+const Helpers = require("./utils/helpers");
 
-app.get("/history/:address", (req, res, next) => {
+app.get("/history/:address/:network", (req, res, next) => {
     res.header( 'Access-Control-Allow-Origin','*' );
     const address = req.params.address;
-    getObjectWithValue(address).then(res.send).catch(res.error);
+    const network = req.params.network;
+    new Helpers(address, network)
+        .getObjectWithValue()
+        .then(res.send)
+        .catch(res.error);
 });
-
-async function getObjectWithValue(address) {
-    const result = await request.post("https://polkadot.api.subscan.io/api/scan/staking_history", {
-        "row": 20,
-        "page": 0,
-        "address": address
-    });
-    for(const index in result.history) {
-        const timestamp = result.history[index].block_timestamp;
-        const reward = result.history[index].reward;
-        const priceAtTime = await getPrice(timestamp);
-        result.history[index].value = priceAtTime * reward;
-    }
-    return result;
-}
-
-async function getPrice(time) {
-    const result = await request.post("https://polkadot.api.subscan.io/api/open/price", {
-        "time": time
-    });
-    return result.data.price;
-}
 
 app.listen(port, () => console.log(`listening at ${port}`));
---
+++ utils/helpers.js
@@ -0,0 +1,42 @@
+const request = require("superagent");
+
+module.exports = class Helpers {
+    
+    constructor(address, network) {
+        const endpoints = {
+            "polkadot": "https://polkadot.api.subscan.io",
+            "kusama": "https://kusama.api.subscan.io"
+        }
+        this.address = address; 
+        this.endpoint = endpoints[network];
+        this.apiKey = "";
+    }
+
+    async getObjectWithValue() {
+        const result = await request.post(`${this.endpoint}/api/scan/staking_history`, {
+            "X-API-Key": this.apiKey,
+            "row": 20,
+            "page": 0,
+            "address": this.address
+        });
+        result.total_value_usd = 0;
+        for(const index in result.history) {
+            const timestamp = result.history[index].block_timestamp;
+            const reward = result.history[index].reward;
+            const priceAtTime = await this.getPrice(timestamp, this.endpoint);
+            const valueUSD = priceAtTime * reward;
+            result.history[index].usd = valueUSD;
+            result.total_value_usd += valueUSD;
+        }
+        return result;
+    }
+
+    async getPrice(time) {
+        const result = await request.post(`${this.endpoint}/api/open/price`, {
+            "X-API-Key": this.apiKey,
+            "time": time
+        });
+        return result.data.price;
+    }
+    
+}
