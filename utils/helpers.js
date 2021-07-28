508bbaae437c54d8d040dd22875e90659989dc83
James Sangalli <j.l.sangalli@gmail.com>
2021-07-28 14:08:52 +1000

    store and get historical prices from JSON files

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,31 +1,45 @@
 const request = require("superagent");
+const dotPrices = require("./prices/usd/dot-prices.json");
+const ksmPrices = require("./prices/usd/ksm-prices.json");
+const endpoints = {
+    "DOT": "https://polkadot.api.subscan.io",
+    "KSM": "https://kusama.api.subscan.io"
+}
+const decimals = {
+    "DOT": 1e10,
+    "KSM": 1e12
+}
 
 module.exports = class Helpers {
     
     constructor(address, network) {
-        const endpoints = {
-            "DOT": "https://polkadot.api.subscan.io",
-            "KSM": "https://kusama.api.subscan.io"
-        }
-        const decimals = {
-            "DOT": 1e10,
-            "KSM": 1e12
-        }
         this.address = address;
         this.network = network;
         this.decimal = decimals[network];
         this.endpoint = endpoints[network];
         this.apiKey = process.env.API_KEY;
+        if(this.network === "DOT") {
+            this.prices = dotPrices;
+        } else {
+            this.prices = ksmPrices;
+        }
     }
 
     async getObjectWithValue() {
-        const result = await request.post(`${this.endpoint}/api/scan/account/reward_slash`, {
-            "X-API-Key": this.apiKey,
-            "row": 20,
-            "page": 0,
-            "address": this.address
-        });
-        return this.handleData(result.body.data);
+        try {
+            const result = await request.post(`${this.endpoint}/api/scan/account/reward_slash`, {
+                "X-API-Key": this.apiKey,
+                "row": 20,
+                "page": 0,
+                "address": this.address
+            });
+            return this.handleData(result.body.data);
+        } catch (e) {
+            console.error(e);
+            // useful if throttled
+            await this.timeout(1000);
+            return this.getObjectWithValue();
+        }
     }
 
     async handleData(result) {
@@ -57,6 +71,21 @@ module.exports = class Helpers {
     }
 
     async getPrice(time, amount, currency) {
+        // coingecko uses zero hour time snapshots
+        const date = new Date(time * 1000).setHours(0, 0, 0, 0);
+        // TODO a bit inefficient to have to iterate each time
+        for(const snapshot of this.prices) {
+            const snapshotTime = new Date(snapshot.snapped_at).setHours(0,0,0,0);
+            if(snapshotTime === date) {
+                const output = parseFloat(snapshot.price).toFixed(2);
+                return parseFloat(output);
+            }
+        }
+        // only used if the price cannot be found in the JSON dump
+        return this.fetchPrices(time, amount, currency);
+    }
+
+    async fetchPrices(time, amount, currency) {
         try {
             const result = await request.post(`${this.endpoint}/api/open/price_converter`, {
                 "X-API-Key": this.apiKey,
@@ -69,10 +98,10 @@ module.exports = class Helpers {
             return parseFloat(output);
         } catch (e) {
             console.error(e);
+            // useful if throttled
             await this.timeout(1000);
             return this.getPrice(time, amount, currency);
         }
-
     }
     
 }
