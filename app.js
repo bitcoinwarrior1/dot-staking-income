57305a22988e2ee7779ed0a5f63a2823989f5f74
James Sangalli <j.l.sangalli@gmail.com>
2022-02-23 09:47:58 +1100

    remove redundant update code (heroku doesn't have a proper storage system)

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const Helpers = require("./utils/helpers");
-const { updatePrices } = require("./utils/prices/update");
 
 app.get("/history/:address/:network/:currency", (req, res, next) => {
     res.header( 'Access-Control-Allow-Origin','*' );
@@ -16,14 +15,6 @@ app.get("/history/:address/:network/:currency", (req, res, next) => {
     });
 });
 
-function pollForPriceUpdates() {
-    setTimeout(() => {
-        updatePrices().then(console.log).catch(console.error);
-        pollForPriceUpdates();
-    }, 72000 * 1000);
-}
-
 app.listen(port, () => {
-    pollForPriceUpdates();
     console.log(`listening at ${port}`)
 });
