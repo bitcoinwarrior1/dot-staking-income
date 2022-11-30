f82b3a0805087cc84a13f1601b87be76d231b22a
James Sangalli <j.l.sangalli@gmail.com>
2022-11-30 14:40:38 +1100

    add date option and route #15

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 const Helpers = require("./utils/helpers");
 
-app.get("/history/:address/:network/:currency", (req, res, next) => {
+app.get("/history/:address/:network/:currency/:fromDate/:toDate", (req, res, next) => {
     res.header( 'Access-Control-Allow-Origin','*' );
     const address = req.params.address;
     const network = req.params.network.toUpperCase();
     const currency = req.params.currency.toLowerCase();
-    new Helpers(address, network, currency).getResults().then((result) => {
+    const fromDate = parseInt(req.params.fromDate);
+    const toDate = parseInt(req.params.toDate);
+    new Helpers(address, network, currency, fromDate, toDate).getResults().then((result) => {
         res.send(result);
     }).catch((err) => {
         res.send(err);
--- test/helpers.test.js
+++ test/helpers.test.js
@@ -19,4 +19,14 @@ describe("helpers functionality", () => {
         expect(result.total_value_DOT !== 0, "should form the total DOT value properly");
     });
 
+    it("should get results within the specified date range", async() => {
+        const from = 1638193558;
+        const to = 1669769171;
+        const result = await new Helpers("13yHcTycP5tJhfDNvzLBDhLLgTEC9ZuZp3sL8yJNsoWh7Fpd", "DOT", "AUD", from, to).getResults();
+        const { list } = result;
+        expect(list.length !== 0, "should return a list of results");
+        expect(list[0].timestamp >= from, "Should be at or later than the from time");
+        expect(list[list.length - 1].timestamp <= to, "Should be at or earlier than the to time");
+    });
+
 });
